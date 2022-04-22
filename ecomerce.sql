69a307c213d5eab23032ce263138ad258b244867
horus100 <77517569+horus100@users.noreply.github.com>
2022-04-22 18:52:44 -0500

    Database

--- ecomerce.sql
+++ ecomerce.sql
@@ -1,8 +1,12 @@
 
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
 
 --
 -- Base de datos: `ecomerce`
@@ -26,7 +30,30 @@ CREATE TABLE `categoria` (
 --
 
 INSERT INTO `categoria` (`ID`, `Nombre`) VALUES
-(1, 'Electrodomesticos');
+(1, 'Electrodomesticos'),
+(2, 'Categoria Nueva'),
+(3, 'Categoria Nueva 10'),
+(4, 'Tecnologia');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `envios`
+--
+
+CREATE TABLE `envios` (
+  `ID` int(11) NOT NULL,
+  `ID_Cliente` int(11) NOT NULL,
+  `ID_Venta` int(11) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `departamento` varchar(300) NOT NULL,
+  `provincia` varchar(250) NOT NULL,
+  `distrito` varchar(300) NOT NULL,
+  `direccion` varchar(300) NOT NULL,
+  `referencia` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -76,7 +103,8 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`ID`, `Nombre`, `Apellido`, `DNI`, `Celular`, `Correo`, `Pw`, `Rol`) VALUES
 (4, 'Usuario', 'nuevo4', '12345678', '10', 'correo1@mail.com', '$2y$10$OeWiWA5VyknWfvCr40Xvre5SM9k6Tf5HuJwhqPmT5hXN10GG1dKnK', ''),
 (5, 'Usuario', '1', '12345678', '12', 'gmail@mail.com', '$2y$10$8PL.ZFMxIAbr0nQUf9GHp.uZ.yZbh9s4XnrEChyO/Z0KgMc6zfRxu', ''),
-(6, 'Usuario', '10', '12345678', '100', 'hola100@mail.com', '$2y$10$HXqF7wjg8Drrx3YphuoMyOJoJ9nFUBuNqx4YIMyBfEJkJwUsDmk7i', '');
+(6, 'Usuario', '10', '12345678', '100', 'hola100@mail.com', '$2y$10$HXqF7wjg8Drrx3YphuoMyOJoJ9nFUBuNqx4YIMyBfEJkJwUsDmk7i', ''),
+(7, 'admin', 'admin', '12', '1', 'admin@admin.com', '$2y$10$kDBtHUF2RhBgElhPR8JxWeXdlrM7aFGDc0KaDD2E6tv9EY3QWjGLS', 'admin');
 
 -- --------------------------------------------------------
 
@@ -92,53 +120,6 @@ CREATE TABLE `ventas` (
   `Estado` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `ventas`
---
-
-INSERT INTO `ventas` (`ID`, `ID_Cliente`, `Monto`, `Fecha`, `Estado`) VALUES
-(2, 6, '30.60', '2022-04-17', 'Pendiente'),
-(3, 6, '40.80', '2022-04-17', 'Pendiente'),
-(4, 6, '40.90', '2022-04-17', 'Pendiente'),
-(5, 6, '51.00', '2022-04-17', 'Pendiente'),
-(6, 4, '10.20', '2022-04-17', 'Pendiente'),
-(10, 4, '10.00', '2022-04-17', 'Pendiente'),
-(11, 4, '10.00', '2022-04-17', 'Pendiente'),
-(12, 4, '10.00', '2022-04-17', 'Pendiente'),
-(13, 4, '10.00', '2022-04-17', 'Pendiente'),
-(14, 4, '10.00', '2022-04-17', 'Pendiente'),
-(15, 4, '10.00', '2022-04-17', 'Pendiente'),
-(16, 4, '10.00', '2022-04-17', 'Pendiente'),
-(17, 4, '10.00', '2022-04-17', 'Pendiente'),
-(18, 4, '10.00', '2022-04-17', 'Pendiente'),
-(19, 4, '10.00', '2022-04-17', 'Pendiente'),
-(20, 4, '10.00', '2022-04-17', 'Pendiente'),
-(21, 4, '10.00', '2022-04-17', 'Pendiente'),
-(22, 4, '10.00', '2022-04-17', 'Pendiente'),
-(23, 4, '10.00', '2022-04-17', 'Pendiente'),
-(24, 4, '10.00', '2022-04-17', 'Pendiente'),
-(25, 4, '10.00', '2022-04-17', 'Pendiente'),
-(26, 4, '10.00', '2022-04-17', 'Pendiente'),
-(27, 4, '10.00', '2022-04-17', 'Pendiente'),
-(28, 4, '10.00', '2022-04-17', 'Pendiente'),
-(29, 4, '10.00', '2022-04-17', 'Pendiente'),
-(30, 4, '10.00', '2022-04-17', 'Pendiente'),
-(31, 4, '10.00', '2022-04-17', 'Pendiente'),
-(32, 4, '10.00', '2022-04-17', 'Pendiente'),
-(33, 4, '10.00', '2022-04-17', 'Pendiente'),
-(34, 4, '10.00', '2022-04-17', 'Pendiente'),
-(35, 4, '10.00', '2022-04-17', 'Pendiente'),
-(36, 4, '10.00', '2022-04-17', 'Pendiente'),
-(37, 4, '10.00', '2022-04-17', 'Pendiente'),
-(38, 4, '10.00', '2022-04-17', 'Pendiente'),
-(39, 4, '10.00', '2022-04-17', 'Pendiente'),
-(41, 5, '20.40', '2022-04-17', 'Pendiente'),
-(42, 5, '20.40', '2022-04-17', 'Pendiente'),
-(43, 5, '20.40', '2022-04-17', 'Pendiente'),
-(44, 5, '30.60', '2022-04-18', 'Pendiente'),
-(45, 4, '10.20', '2022-04-18', 'Pendiente'),
-(46, 5, '20.40', '2022-04-18', 'Pendiente');
-
 -- --------------------------------------------------------
 
 --
@@ -153,31 +134,6 @@ CREATE TABLE `venta_productos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `venta_productos`
---
-
-INSERT INTO `venta_productos` (`ID`, `ID_Venta`, `ID_Producto`, `Cantidad`) VALUES
-(1, 4, 1, 1),
-(2, 4, 1, 1),
-(3, 4, 1, 1),
-(4, 4, 1, 1),
-(5, 5, 1, 1),
-(6, 5, 1, 1),
-(7, 5, 1, 1),
-(8, 5, 1, 1),
-(9, 5, 1, 1),
-(10, 6, 1, 1),
-(31, 42, 1, 1),
-(32, 42, 1, 1),
-(33, 43, 1, 1),
-(34, 43, 1, 1),
-(35, 44, 1, 1),
-(36, 44, 1, 1),
-(37, 44, 1, 1),
-(38, 45, 1, 1),
-(39, 46, 1, 2);
-
---
 -- Índices para tablas volcadas
 --
 
@@ -186,6 +142,14 @@ INSERT INTO `venta_productos` (`ID`, `ID_Venta`, `ID_Producto`, `Cantidad`) VALU
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `envios`
+--
+ALTER TABLE `envios`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ID_Cliente` (`ID_Cliente`),
+  ADD KEY `ID_Venta` (`ID_Venta`);
 
 --
 -- Indices de la tabla `productos`
@@ -223,35 +187,48 @@ ALTER TABLE `venta_productos`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `envios`
+--
+ALTER TABLE `envios`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT de la tabla `venta_productos`
 --
 ALTER TABLE `venta_productos`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `envios`
+--
+ALTER TABLE `envios`
+  ADD CONSTRAINT `envios_ibfk_1` FOREIGN KEY (`ID_Cliente`) REFERENCES `usuario` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `envios_ibfk_2` FOREIGN KEY (`ID_Venta`) REFERENCES `ventas` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `productos`
