288030dffe23addc34a9834b31a3d5ebc0ca3046
Jhonny Quispe <jhonny.quispejl@gmail.com>
2022-05-19 21:00:04 -0500

    config template and add custom styles

--- Vista/inicio.php
+++ Vista/inicio.php
@@ -1,34 +1,85 @@
+<div class="app-content">
+    <div class="bg-anti-flash-white">
+        <div class="white-container">
+            <div class="u-s-p-b-60">
+                <div class="section__intro u-s-m-b-46">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="section__text-wrap">
+                                    <h2 class="section__heading u-c-secondary u-s-m-b-12">Lo más vendido</h2>
 
-    <?php
-        if (isset($lista)) {
-            //echo " Hay una lista";
-            if(isset($mensaje)){
-                echo "<h1>".$mensaje."</h1> ";
-            }
-            foreach($lista as $producto) { ?>
-
-                <div class="col-3">
-                    <div class="card">
-                        <img height="317px" title="<?php echo $producto['Nombre']?>" class="card-img-top" src="<?php echo $producto['Imagen']?>" alt="<?php echo $producto['Nombre']?>">
-                        <div class="card-body">
-                            <span><?php echo $producto['Nombre']?></span>
-                            <h5 class="card-title">S/ <?php echo $producto['Precio_und']?></h5>
-                            <form action="<?=url_index?>index.php?controlador=Carrito&accionar=funcion"  method="post">
-
-                                <input type="hidden" name="id" id="id" value="<?php echo openssl_encrypt(intval($producto['ID']),COD,KEY);?>">
-                                <input type="hidden" name="nombre" id="nombre" value="<?php echo openssl_encrypt($producto['Nombre'],COD,KEY);?>">
-                                <input type="hidden" name="precio" id="precio" value="<?php echo openssl_encrypt($producto['Precio_und'],COD,KEY);?>">
-                                <input type="hidden" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt(1,COD,KEY);?>">
-
-                                <button class="btn btn-primary" name="btnAccion" value="Agregar" type="submit">Agregar al carrito</button>
-                            </form>
+                                    <span class="section__span u-c-silver u-s-m-b-16">Llena tu bolsa con nuestros mejores productos</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            <?php } 
-            
-        } else {
-            echo "<h1> No hay una lista</h1>";
-        }
-        
-    ?>
+
+                <div class="section__content">
+                    <div class="container">
+                        <div class="row">
+                            <?php
+                                if (isset($lista)) {
+                                    //echo " Hay una lista";
+                                    if(isset($mensaje)){
+                                        echo "<h3 class='text-error'>".$mensaje."</h3> ";
+                                    }
+                                    foreach($lista as $producto) { ?>
+                                        <!-- card -->
+                                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 u-s-m-b-30">
+                                            <div class="product-o2 u-h-100">
+                                                <div class="product-o2__wrap">
+                                                    <a class="aspect aspect--bg-grey aspect--square u-d-block" href="#">
+                                                        <img class="aspect__img product-o2__img"  src="<?php echo $producto['Imagen']?>" alt="<?php echo $producto['Nombre']?>">
+                                                    </a>
+                                                </div>
+
+                                                <div class="product-o2__content">
+                                                    <span class="product-o2__name">
+                                                        <a href="#"><?php echo $producto['Nombre']?></a>
+                                                    </span>
+                                                    <span class="product-o2__category">
+                                                        <a href="#"><?php echo $producto['Descripcion'] ?></a>
+                                                    </span>
+                                                    <span class="product-o2__price">
+                                                        S/ <?php echo $producto['Precio_und']?>
+                                                    </span>
+                                                    <span class="product-o2__action">
+                                                        <button class="nav-link btn btn--e-brand">Agregar a la bolsa</button>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- card -->
+                                    <?php } 
+                                } else {
+                                    echo "<p class='text-info'> No hay productos disponibles</p>";
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- <div class="col-3">
+    <div class="card">
+        <img height="317px" title="<?php echo $producto['Nombre']?>" class="card-img-top" src="<?php echo $producto['Imagen']?>" alt="<?php echo $producto['Nombre']?>">
+        <div class="card-body">
+            <span><?php echo $producto['Nombre']?></span>
+            <h5 class="card-title">S/ <?php echo $producto['Precio_und']?></h5>
+            <form action="<?=url_index?>index.php?controlador=Carrito&accionar=funcion"  method="post">
+
+                <input type="hidden" name="id" id="id" value="<?php echo openssl_encrypt(intval($producto['ID']),COD,KEY);?>">
+                <input type="hidden" name="nombre" id="nombre" value="<?php echo openssl_encrypt($producto['Nombre'],COD,KEY);?>">
+                <input type="hidden" name="precio" id="precio" value="<?php echo openssl_encrypt($producto['Precio_und'],COD,KEY);?>">
+                <input type="hidden" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt(1,COD,KEY);?>">
+
+                <button class="btn btn-primary" name="btnAccion" value="Agregar" type="submit">Agregar al carrito</button>
+            </form>
+        </div>
+    </div>
+</div> -->
--- Vista/nav.php
+++ Vista/nav.php
@@ -1,39 +1,59 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
+    <title>FRUTALINA</title>
+    <!-- import styles -->
+    <link rel="stylesheet" href="./assets/css/vendor.css">
+    <link rel="stylesheet" href="./assets/css/utility.css">
+    <link rel="stylesheet" href="./assets/css/app.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/css/card.css">
+    <!-- import styles -->
 </head>
-<body>
-    <ul>
-        <li><a href="<?=url_index?>">Inicio</a></li>
-        <li><a href="<?=url_index?>index.php?controlador=carrito&accionar=index">Carrito</a></li>
-        <?php if (GeneralControl::V_Sesion("usuario") || GeneralControl::V_Sesion("admin")) {
-                if (GeneralControl::V_Sesion("usuario")) {
-                    $nombre=$_SESSION['usuario']['Nombre'];
-                } else {
-                    $nombre=$_SESSION['admin']['Nombre'];
-                }
+
+<body class="config" id="js-scrollspy-trigger">
+    <!-- loader -->
+    <div class="preloader is-active">
+        <div class="preloader__wrap">
+
+        <img class="preloader__img" src="https://raw.githubusercontent.com/ahmadhuss/ludus-free-premium-ecommerce-template/master/images/preloader.png" alt="loader"></div>
+    </div>
+    <!-- loader -->
+
+
+    <!-- <div id="app">
+        <ul>
+            <li><a href="<?=url_index?>">Inicio</a></li>
+            <li><a href="<?=url_index?>index.php?controlador=carrito&accionar=index">Carrito</a></li>
+            <?php if (GeneralControl::V_Sesion("usuario") || GeneralControl::V_Sesion("admin")) {
+                    if (GeneralControl::V_Sesion("usuario")) {
+                        $nombre=$_SESSION['usuario']['Nombre'];
+                    } else {
+                        $nombre=$_SESSION['admin']['Nombre'];
+                    }
+                    
+                ?>
+                <h1><?php echo "Hola ".$nombre; ?></h1>
+                <li><a href="<?=url_index?>index.php?controlador=compra&accionar=index">Compras</a></li>            
+                <li><a href="<?=url_index?>index.php?controlador=usuario&accionar=cerrar_sesion">cerrar sesion</a></li>
+                <?php 
+                    if(GeneralControl::V_Sesion("admin")){ ?>
+
+                <li><a href="<?=url_index?>index.php?controlador=venta&accionar=index">ver ventas</a></li>            
+                <li><a href="<?=url_index?>index.php?controlador=producto&accionar=crear_producto">crear producto</a></li>            
+                <li><a href="<?=url_index?>index.php?controlador=producto&accionar=crear_Categoria">crear categoria</a></li>
+    
+
+                <?php  }
                 
-            ?>
-            <h1><?php echo "Hola ".$nombre; ?></h1>
-            <li><a href="<?=url_index?>index.php?controlador=compra&accionar=index">Compras</a></li>            
-            <li><a href="<?=url_index?>index.php?controlador=usuario&accionar=cerrar_sesion">cerrar sesion</a></li>
-            <?php 
-                if(GeneralControl::V_Sesion("admin")){ ?>
+                ?>
+            <?php } else{?> 
+                <li><a href="<?=url_index?>index.php?controlador=usuario&accionar=index">Login</a></li>
+                <li><a href="<?=url_index?>index.php?controlador=usuario&accionar=registro">Registro</a></li>  
+            <?php } ?>
+        </ul>-->
 
-            <li><a href="<?=url_index?>index.php?controlador=venta&accionar=index">ver ventas</a></li>            
-            <li><a href="<?=url_index?>index.php?controlador=producto&accionar=crear_producto">crear producto</a></li>            
-            <li><a href="<?=url_index?>index.php?controlador=producto&accionar=crear_Categoria">crear categoria</a></li>
- 
-
-            <?php  }
-            
-            ?>
-        <?php } else{?> 
-            <li><a href="<?=url_index?>index.php?controlador=usuario&accionar=index">Login</a></li>
-            <li><a href="<?=url_index?>index.php?controlador=usuario&accionar=registro">Registro</a></li>  
-        <?php } ?>
-    </ul>
+        
