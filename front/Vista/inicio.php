9e1f96e1eadc0f890066c2de114f2890d33e9c31
Jhonny Quispe <jhonny.quispejl@gmail.com>
2022-06-15 08:31:45 -0500

    slider works

--- front/Vista/inicio.php
+++ front/Vista/inicio.php
@@ -1,3 +1,205 @@
+<div class="app-content">
+    <div class="bg-anti-flash-white">
+        <div class="white-container">
+            <!-- carrousel -->
+            <div class="container u-s-p-b-60">
+                <div class="s-skeleton s-skeleton--h-600 s-skeleton--bg-black">
+                    <div class="owl-carousel primary-style-2" id="hero-slider">
+                        <!-- slider 1 -->
+                        <div class="hero-slide hero-slide--4">
+                            <div class="primary-style-2-container">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="slider-content slider-content--animation">
+                                            <span class="content-span-1 u-c-white">Disfruta de nuestros deliciosos</span>
+
+                                            <span class="content-span-2 u-c-white">jugos surtidos</span>
+
+                                            <span class="content-span-3 u-c-white">Los mejores jugos los encuentras en la Frutalina,deliciosos y con una gran tradición</span>
+
+                                            <span class="content-span-4 u-c-white">
+                                                Desde
+                                                <span class="u-c-brand">$5.00</span>
+                                            </span>
+
+                                            <a class="shop-now-link btn--e-brand" href="#">Ver más</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- slider 1 -->
+
+                        <!-- slider 2 -->
+                        <div class="hero-slide hero-slide--4 hero-slide hero-slide--5">
+                            <div class="primary-style-2-container">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="slider-content slider-content--animation">
+                                            <span class="content-span-1 u-c-white">Disfruta de nuestros deliciosos</span>
+
+                                            <span class="content-span-2 u-c-white">jugos surtidos</span>
+
+                                            <span class="content-span-3 u-c-white">Los mejores jugos los encuentras en la Frutalina,deliciosos y con una gran tradición</span>
+
+                                            <span class="content-span-4 u-c-white">
+                                                Desde
+                                                <span class="u-c-brand">$5.00</span>
+                                            </span>
+
+                                            <a class="shop-now-link btn--e-brand" href="#">Ver más</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- slider 2 -->
+                    </div>
+                </div>
+            </div>
+            <!-- carrousel -->
+
+            <!-- productos más vendidos -->
+            <div class="u-s-p-b-60">
+                <div class="section__intro u-s-m-b-46">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="section__text-wrap">
+                                    <h2 class="section__heading u-c-secondary u-s-m-b-12">Lo más vendido</h2>
+
+                                    <span class="section__span u-c-silver u-s-m-b-16">Llena tu bolsa con nuestros mejores productos</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section__content">
+                    <div class="container">
+                        <div class="row">
+                            <!-- card -->
+                            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 u-s-m-b-30">
+                                <div class="product-o2 u-h-100">
+                                    <div class="product-o2__wrap">
+                                        <a class="aspect aspect--bg-grey aspect--square u-d-block" href="#">
+                                            <img class="aspect__img product-o2__img"  src="<?php echo url_index?>/front/assets/images/logo.svg">
+                                        </a>
+
+                                        <div class="product-o2__action-wrap">
+                                            <ul class="product-o2__action-list">
+                                                <li>
+                                                    <a data-modal="modal" data-toggle="tooltip" data-placement="top" title="Ver detalle" href="#">
+                                                        <i class="fas fa-eye"></i>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div class="product-o2__content">
+                                        <span class="product-o2__name">
+                                            <a href="#">Nombre del producto</a>
+                                        </span>
+                                        <span class="product-o2__category">
+                                            <a href="#">Descripción corta del producto</a>
+                                        </span>
+                                        <span class="product-o2__price">
+                                            S/ 50.00
+                                        </span>
+                                        <span class="product-o2__action">
+                                            <button
+                                                class="nav-link btn btn--e-brand"
+                                            >Agregar a la bolsa</button>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- card -->
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section__more">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <a class="nav-link btn--e-brand" href="#">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- productos más vendidos -->
+            
+            <!-- categorias -->
+            <div class="u-s-p-b-60">
+                <div class="section__intro u-s-m-b-46">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="section__text-wrap">
+                                    <h2 class="section__heading u-c-secondary u-s-m-b-12">Categorias</h2>
+
+                                    <span class="section__span u-c-silver u-s-m-b-16">Descubre todo lo que tenemos para ofrecerte</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section__content">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 u-s-m-b-30">
+                                <div class="category-o">
+                                    <div class="aspect aspect--bg-grey aspect--square category-o__img-wrap">
+                                        <img class="aspect__img category-o__img" src="https://cdn.pixabay.com/photo/2017/01/20/14/59/orange-1995044__340.jpg" alt="">
+                                    </div>
+                                    <div class="category-o__info">
+                                        <a class="category-o__shop-now btn--e-white-brand" href="shop-side-version-2.html">Jugos</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 u-s-m-b-30">
+                                <div class="category-o">
+                                    <div class="aspect aspect--bg-grey aspect--square category-o__img-wrap">
+                                        <img class="aspect__img category-o__img" src="https://cdn.pixabay.com/photo/2016/11/22/18/52/cake-1850011__340.jpg" alt="">
+                                    </div>
+                                    <div class="category-o__info">
+                                        <a class="category-o__shop-now btn--e-white-brand" href="shop-side-version-2.html">Tortas</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 u-s-m-b-30">
+                                <div class="category-o">
+                                    <div class="aspect aspect--bg-grey aspect--square category-o__img-wrap">
+                                        <img class="aspect__img category-o__img" src="https://cdn.pixabay.com/photo/2017/02/09/14/09/kebab-2052491__340.jpg" alt="">
+                                    </div>
+                                    <div class="category-o__info">
+                                        <a class="category-o__shop-now btn--e-white-brand" href="shop-side-version-2.html">Hamburguesas</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 u-s-m-b-30">
+                                <div class="category-o">
+                                    <div class="aspect aspect--bg-grey aspect--square category-o__img-wrap">
+                                        <img class="aspect__img category-o__img" src="https://cdn.pixabay.com/photo/2016/11/06/23/16/breakfast-1804436__340.jpg" alt="">
+                                    </div>
+                                    <div class="category-o__info">
+                                        <a class="category-o__shop-now btn--e-white-brand" href="shop-side-version-2.html">Desayunos</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- categorias -->
+        </div>
+    </div>
+</div>
+
 <?php
         // Comprueba si existe una lista para mostrar
         if (isset($lista)) {
